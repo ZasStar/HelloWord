f55c5f2dfc71865e5196c0159da1fba70237be5c
Alex <zas_st@inbox.ru>
2023-03-14 20:07:32 +0300

    Initial Commit

---
+++ HelloWord/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWord
+//
+//  Created by Александр Захлыпа on 14.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+  
+
